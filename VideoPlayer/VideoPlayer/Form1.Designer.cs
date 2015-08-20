7379412f6680b82d2b890c397d82fba61b6d53d8
DmitryFilippow <Dikoobraz_art@mail.ru>
2015-08-20 16:26:59 +0600

    Простенький Видео плеер

---
+++ VideoPlayer/VideoPlayer/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace VideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label0 = new System.Windows.Forms.Label();
+            this.VolumeTrackBar = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(373, 322);
+            this.panel1.TabIndex = 0;
+            this.panel1.DoubleClick += new System.EventHandler(this.panel1_DoubleClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label0);
+            this.panel2.Controls.Add(this.VolumeTrackBar);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Controls.Add(this.OpenButton);
+            this.panel2.Controls.Add(this.StopButton);
+            this.panel2.Controls.Add(this.PlayButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 224);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(373, 98);
+            this.panel2.TabIndex = 1;
+            // 
+            // label0
+            // 
+            this.label0.AutoSize = true;
+            this.label0.Location = new System.Drawing.Point(99, 34);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(13, 13);
+            this.label0.TabIndex = 6;
+            this.label0.Text = "0";
+            // 
+            // VolumeTrackBar
+            // 
+            this.VolumeTrackBar.Location = new System.Drawing.Point(12, 34);
+            this.VolumeTrackBar.Name = "VolumeTrackBar";
+            this.VolumeTrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.VolumeTrackBar.Size = new System.Drawing.Size(42, 61);
+            this.VolumeTrackBar.TabIndex = 5;
+            this.VolumeTrackBar.Scroll += new System.EventHandler(this.VolumeTrackBar_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.trackBar1.Location = new System.Drawing.Point(0, 0);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(373, 42);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(249, 50);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 2;
+            this.OpenButton.Text = "Открыть";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(168, 50);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Стоп";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(87, 50);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(75, 23);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "Играть";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bw_ProgressChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 322);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Video player";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar VolumeTrackBar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label0;
+    }
+}
+
---
+++ VideoPlayer/VideoPlayer/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.AudioVideoPlayback;
+using Timer = System.Threading.Timer;
+
+namespace VideoPlayer
+{
+    public partial class Form1 : Form
+    {
+        public OpenFileDialog openFileDialog1;
+        public Video video;
+        public int VideoState = 0;
+        public int Min, Hour, Sec;
+        //public BackgroundWorker bw=new BackgroundWorker();
+
+
+        private enum VideoStateEnum
+        {
+            None,
+            Play,
+            Pause,
+            Stop,
+            Open
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+
+        }
+
+
+        public void SeekRefresh()
+        {
+            //ChatBox.BeginInvoke(AcceptDelegate, new object[] { "Received " + Encoding.Default.GetString(MessageR.ToArray()), ChatBox });
+
+            label1.Text = video.CurrentPosition.ToString();
+            //trackBar1.Value=(int)video.CurrentPosition;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //VideoState = (int)VideoStateEnum.Open;
+
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Видео файл (*.avi)|*.avi|Видео файл (*.wmv)|*.wmv|Видео файл (*.mp4)|*.mp4|Все файлы (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                FileInfo fi = new FileInfo(openFileDialog1.FileName);
+                if (fi.Length > 0)
+                {
+                    if (video != null &&
+                        (VideoState == (int) VideoStateEnum.Stop || VideoState == (int) VideoStateEnum.Play))
+                    {
+                        video.Stop();
+                    }
+                    if (video == null) backgroundWorker1.RunWorkerAsync();
+
+                        video = new Video(openFileDialog1.FileName);
+                        video.Open(openFileDialog1.FileName);
+                        video.Owner = panel1;
+                        VideoState = (int) VideoStateEnum.Stop;
+                        label1.Text = video.Duration.ToString();
+                        label2.Text = video.StopPosition.ToString();
+                        trackBar1.Maximum = (int) video.Duration;
+                        trackBar1.TickFrequency = (int) video.Duration/100;
+                        VolumeTrackBar.Minimum = -5000;
+                        VolumeTrackBar.Maximum = 0;
+                        VolumeTrackBar.TickFrequency = VolumeTrackBar.Minimum/10;
+                        video.Audio.Volume = VolumeTrackBar.Minimum/2;
+                        VolumeTrackBar.Value = video.Audio.Volume;
+
+                        video.StopWhenReady();
+
+                }
+            }
+            else
+            {
+                VideoState = (int) VideoStateEnum.None;
+            }
+
+        }
+
+        public void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            BackgroundWorker worker = sender as BackgroundWorker;
+            //for (int i = 1; i <= 10; i++)
+            for (;;)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    // Perform a time consuming operation and report progress.
+                    System.Threading.Thread.Sleep(1000);
+                    try
+                    {
+                        worker.ReportProgress((int) video.CurrentPosition);
+                    }
+                    catch (Exception)
+                    {
+                        worker.ReportProgress(0);
+                    }
+
+                }
+            }
+        }
+
+        public int cs = 0;
+
+        public void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Min = (int) Math.Floor(video.CurrentPosition/60);
+            Hour = (int) Math.Floor((double) (Min/60));
+            Sec = (int) Math.Round(video.CurrentPosition);
+            label0.Text = Hour.ToString();
+            label1.Text = Min.ToString();
+            label2.Text = Sec.ToString();
+            trackBar1.Value = (int)video.CurrentPosition;
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private delegate void SendMsg(String Text, Label Rtb);
+
+        private SendMsg AcceptDelegate = (String Text, Label Rtb) => { Rtb.Text = Text; };
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (VideoState != (int) VideoStateEnum.None)
+            {
+                if (video.Playing)
+                {
+                    PlayButton.Text = "Играть";
+                    video.Stop();
+                    trackBar1.Value = trackBar1.Minimum;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (video != null)
+            {
+                if (video.Playing)
+                {
+                    PlayButton.Text = "Играть";
+                    video.Pause();
+                    VideoState = (int) VideoStateEnum.Pause;
+                    return;
+                }
+                else
+                {
+                    PlayButton.Text = "Пауза";
+                    video.Play();
+                    VideoState = (int) VideoStateEnum.Play;
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (video != null) video.CurrentPosition = trackBar1.Value;
+
+        }
+
+        private void VolumeTrackBar_Scroll(object sender, EventArgs e)
+        {
+            if (video != null) video.Audio.Volume = VolumeTrackBar.Value;
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void panel1_DoubleClick(object sender, EventArgs e)
+        {
+         
+            
+        }
+
+
+        
+      
+    }
+}
+
