6778c269915f4b96a79f0d9b38c31f24e4467a06
DmitryFilippow <Dikoobraz_art@mail.ru>
2015-08-20 17:12:05 +0600

    Win info program

---
+++ WindowsInfos/WindowsInfos/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsInfos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RB = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TempListBox = new System.Windows.Forms.ListBox();
+            this.GTButton = new System.Windows.Forms.Button();
+            this.HDDTempTB = new System.Windows.Forms.RichTextBox();
+            this.TestTB = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RB
+            // 
+            this.RB.Location = new System.Drawing.Point(12, 22);
+            this.RB.Name = "RB";
+            this.RB.Size = new System.Drawing.Size(312, 179);
+            this.RB.TabIndex = 0;
+            this.RB.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Информация о сети";
+            // 
+            // TempListBox
+            // 
+            this.TempListBox.FormattingEnabled = true;
+            this.TempListBox.Location = new System.Drawing.Point(15, 237);
+            this.TempListBox.Name = "TempListBox";
+            this.TempListBox.Size = new System.Drawing.Size(149, 95);
+            this.TempListBox.TabIndex = 2;
+            // 
+            // GTButton
+            // 
+            this.GTButton.Location = new System.Drawing.Point(194, 339);
+            this.GTButton.Name = "GTButton";
+            this.GTButton.Size = new System.Drawing.Size(120, 27);
+            this.GTButton.TabIndex = 3;
+            this.GTButton.Text = "Get info";
+            this.GTButton.UseVisualStyleBackColor = true;
+            this.GTButton.Click += new System.EventHandler(this.GTButton_Click);
+            // 
+            // HDDTempTB
+            // 
+            this.HDDTempTB.Location = new System.Drawing.Point(170, 237);
+            this.HDDTempTB.Name = "HDDTempTB";
+            this.HDDTempTB.Size = new System.Drawing.Size(154, 96);
+            this.HDDTempTB.TabIndex = 4;
+            this.HDDTempTB.Text = "";
+            // 
+            // TestTB
+            // 
+            this.TestTB.Location = new System.Drawing.Point(12, 372);
+            this.TestTB.Name = "TestTB";
+            this.TestTB.Size = new System.Drawing.Size(319, 74);
+            this.TestTB.TabIndex = 5;
+            this.TestTB.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Температура  CPUs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Температура HDDs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 352);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Информация о ресурсах";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 447);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TestTB);
+            this.Controls.Add(this.HDDTempTB);
+            this.Controls.Add(this.GTButton);
+            this.Controls.Add(this.TempListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RB);
+            this.Name = "Form1";
+            this.Text = "WinInfo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox TempListBox;
+        private System.Windows.Forms.Button GTButton;
+        private System.Windows.Forms.RichTextBox HDDTempTB;
+        private System.Windows.Forms.RichTextBox TestTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+
+
+    }
+}
+
---
+++ WindowsInfos/WindowsInfos/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInfos.Properties;
+using System.Management.Instrumentation;
+using System.Diagnostics;
+
+namespace WindowsInfos
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefrInfo();
+        }
+
+        public void RefrInfo()
+        {
+            ShowNetworkInterfaces();
+            ShowTemp();
+            ShowHDDTemp();
+            CounterInfo();
+        }
+        
+        public  void ShowNetworkInterfaces()
+        {
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            ConsoleWriteLine(computerProperties.HostName.ToString());
+            ConsoleWriteLine(computerProperties.DomainName.ToString());
+            if (nics == null || nics.Length < 1)
+            {
+                ConsoleWriteLine("  No network interfaces found.");
+                return;
+            }
+
+            ConsoleWriteLine(nics.Length.ToString());
+            foreach (NetworkInterface adapter in nics)
+            {
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+            
+                ConsoleWriteLine(adapter.Description);
+                ConsoleWriteLine(String.Empty.PadLeft(adapter.Description.Length, '='));
+                ConsoleWriteLine(adapter.NetworkInterfaceType.ToString());
+                ConsoleWriteLine(adapter.GetPhysicalAddress().ToString());
+                ConsoleWriteLine(adapter.OperationalStatus.ToString());
+                string versions = "";
+
+                // Create a display string for the supported IP versions.
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    versions = "IPv4";
+                }
+                if (adapter.Supports(NetworkInterfaceComponent.IPv6))
+                {
+                    if (versions.Length > 0)
+                    {
+                        versions += " ";
+                    }
+                    versions += "IPv6";
+                }
+                ConsoleWriteLine(versions.ToString());
+                ConsoleWriteLine(properties.ToString());
+                ShowIPAddresses(properties);
+
+                // The following information is not useful for loopback adapters.
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                {
+                    continue;
+                }
+                ConsoleWriteLine(properties.DnsSuffix.ToString());
+
+                string label;
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    IPv4InterfaceProperties ipv4 = properties.GetIPv4Properties();
+                    ConsoleWriteLine(ipv4.Mtu.ToString());
+                    if (ipv4.UsesWins)
+                    {
+
+                        IPAddressCollection winsServers = properties.WinsServersAddresses;
+                        if (winsServers.Count > 0)
+                        {
+                            foreach (var winsServer in winsServers)
+                            {
+                                ConsoleWriteLine(winsServer.ToString());
+                            }
+                        }
+                    }
+                }
+
+                ConsoleWriteLine(properties.IsDnsEnabled.ToString());
+                ConsoleWriteLine(properties.IsDynamicDnsEnabled.ToString());
+                ConsoleWriteLine(adapter.IsReceiveOnly.ToString());
+                ConsoleWriteLine(adapter.SupportsMulticast.ToString());
+                ShowInterfaceStatistics(adapter);
+
+                
+            }
+
+        }
+
+        private static void ShowIPAddresses(IPInterfaceProperties properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowIPAddresses(string label, IPAddressCollection winsServers)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowInterfaceStatistics(NetworkInterface adapter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConsoleWriteLine(string text)
+        {
+            RB.AppendText(String.Format("{0}\n",text));
+        }
+        public void ConsoleWriteLine(RichTextBox RTB,string text)
+        {
+            RTB.AppendText(String.Format("{0}\n", text));
+        }
+
+        private void GTButton_Click(object sender, EventArgs e)
+        {
+            ShowTemp();
+            ShowHDDTemp();
+            CounterInfo();
+            
+        }
+        private void ShowHDDTemp()
+        {
+            HDDTempTB.Clear();
+            int i = 0;
+            foreach (var vartemp in GetHDDTemp())
+            {
+                
+                ConsoleWriteLine(HDDTempTB,String.Format("HDD №{0} = {1}*C",i,vartemp.ToString()));
+                i++;
+            }
+        }
+
+        private ManagementObjectSearcher MOS;
+        private const byte TEMP_HDD = 194;
+        private List<byte> GetHDDTemp()
+        {
+            List<byte> rval=new List<byte>();
+            try
+            {
+                MOS =new ManagementObjectSearcher("root\\WMI", "SELECT * FROM MSStorageDriver_ATAPISmartData");
+                foreach (var qobj in MOS.Get())
+                {
+                    byte[] arrVS=(byte[]) qobj.GetPropertyValue("VendorSpecific");
+                    int tindex = Array.IndexOf(arrVS, TEMP_HDD);
+                    rval.Add(arrVS[tindex+5]);
+                }
+            }
+            catch (ManagementException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return rval;
+        }
+        private void ShowTemp()
+        {
+            TempListBox.Items.Clear();
+            try
+            {
+                double CPUtprt = 0;
+                MOS=new ManagementObjectSearcher("root\\WMI","SELECT * FROM MSAcpi_ThermalZoneTemperature");
+                foreach (ManagementObject MO in MOS.Get())
+                {
+                    double t, kel, cel, far;
+                    double.TryParse(MO["CurrentTemperature"].ToString(), out t);
+                    kel = t/10;
+                    cel = kel - 273.15;
+                    far = cel*9/5 + 32;
+                    CPUtprt =
+                        Convert.ToDouble(Convert.ToDouble(MO.GetPropertyValue("CurrentTemperature".ToString()))- 2732)/6.96;
+                    //TempListBox.Items.Add(" CPU: " + CPUtprt.ToString() + " *C");
+                    TempListBox.Items.Add(" CPU: " + cel.ToString() + " *C");
+                }
+                
+            }
+            catch (ManagementException ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        private PerformanceCounter m_memCounter;
+        private PerformanceCounter m_cpuCounter;
+        public void CounterInfo()
+        {
+            
+            m_memCounter=new PerformanceCounter();
+            m_memCounter.CategoryName = "Memory";
+            m_memCounter.CounterName = "Available MBytes";
+            m_cpuCounter=new PerformanceCounter();
+            m_cpuCounter.CategoryName = "Processor";
+            m_cpuCounter.CounterName = "% Processor Time";
+            m_cpuCounter.InstanceName = "_Total";
+            TestTB.Clear();
+            ConsoleWriteLine(TestTB,m_memCounter.CategoryName.ToString());
+            ConsoleWriteLine(TestTB,String.Format ("{0} {1}",m_memCounter.CounterName.ToString(), GetAMem().ToString()));
+            ConsoleWriteLine(TestTB,m_cpuCounter.CategoryName.ToString());
+            ConsoleWriteLine(TestTB,String.Format("{0} {1}", m_cpuCounter.CounterName.ToString(), GetCPUL().ToString()));
+        }
+        public float GetAMem()
+        {
+            return m_memCounter.NextValue();
+        }
+        public float GetCPUL()
+        {
+            return m_cpuCounter.NextValue();
+        }
+
+        [DllImport("ntdll.dll")]
+        public static extern int NtSetInformationProcess(IntPtr p, int c,ref int i, int l);
+/*        public void _BSOD()
+        {
+            Process p = Process.GetCurrentProcess();
+            int g = 1;
+            NtSetInformationProcess(p.Handle, 29,ref g, 4);
+            p.Kill();
+        }
+ */
+    }
+}
